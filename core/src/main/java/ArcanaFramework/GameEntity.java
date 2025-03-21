821b205c33103b971dabc411d0893d46bf6eeaed
Ramiro Garcia <67532049+RamiroGarciaAr@users.noreply.github.com>
2025-03-21 11:48:50 -0300

    Create GameEntity.java

---
+++ core/src/main/java/ArcanaFramework/GameEntity.java
@@ -0,0 +1,80 @@
+package ArcanaFramework;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public abstract class GameEntity {
+    //Transform variables
+    protected Vector2 position;
+    protected Vector2 size;
+    protected float rotation;
+    //Visibility - Controls if we hide the element
+    protected boolean visible;
+    protected Texture texture;
+
+    public GameEntity(Vector2 position, Vector2 size, float rotation, Texture texture)
+    {
+        this.position  = position;
+        this.size      = size;
+        this.rotation  = rotation;
+        this.visible   = true;
+        this.texture   = texture;
+    }
+    public GameEntity(Vector2 position, Vector2 size, float rotation, Texture texture, boolean visible)
+    {
+        this.position  = position;
+        this.size      = size;
+        this.rotation  = rotation;
+        this.visible   = visible;
+        this.texture   = texture;
+    }
+    //SETTERS
+    public void setPosition(float x, float y)
+    {
+        position.set(x, y);
+    }
+    public void setRotation(float rotation)
+    {
+        this.rotation = rotation;
+    }
+    public void setVisibility(boolean visible)
+    {
+        this.visible = visible;
+    }
+    public void setTexture(Texture texture)
+    {
+        this.texture = texture;
+    }
+    //GETTERS
+    public Vector2 getPosition()
+    {
+        return position;
+    }
+    public Vector2 getSize()
+    {
+        return size;
+    }
+    public float getRotation()
+    {
+        return rotation;
+    }
+    //Functions
+    public void draw(SpriteBatch batch) {
+        if (texture != null) {
+            batch.draw(texture, position.x, position.y, size.x / 2, size.y / 2, size.x, size.y, 1, 1, rotation, 0, 0,
+                texture.getWidth(), texture.getHeight(), false, false);
+        }
+    }
+
+    public boolean isHovering(float mouseX, float mouseY)
+    {
+        return mouseX >= position.x && mouseX <= position.x + size.x && mouseY >= position.y && mouseY <= position.y + size.y;
+    }
+
+    //Update Function is called each frame
+    //@delta: time passed between frames
+    public abstract void update(float delta);
+
+}
