a5fcb4beacd2c9d2a913718367a89fba04dbca66
Ramiro Garcia <67532049+RamiroGarciaAr@users.noreply.github.com>
2025-06-02 02:28:45 -0300

    Create Component.java

---
+++ core/src/main/java/com/arcana/game/core/ecs/Component.java
@@ -0,0 +1,9 @@
+package com.arcana.game.core.ecs;
+
+/**
+ * Base interface for all ECS components.
+ * Components are pure data containers in the ECS architecture.
+ */
+public interface Component {
+    // Marker interface - no methods required
+} 
