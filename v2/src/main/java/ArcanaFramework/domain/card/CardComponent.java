d499779ebe31c2d4e19e361aaf3165f65ad02db3
Tobias Juhasz <tjuhasz@itba.edu.ar>
2025-06-23 03:25:20 -0300

    Modifica las clases CardComponent y CardEntity para incluir nuevos atributos: symbol, image y type. Se actualiza el constructor de CardEntity para inicializar estos atributos al crear una nueva instancia de CardComponent.

--- v2/src/main/java/ArcanaFramework/domain/card/CardComponent.java
+++ v2/src/main/java/ArcanaFramework/domain/card/CardComponent.java
@@ -3,7 +3,11 @@ package ArcanaFramework.domain.card;
 import ArcanaFramework.core.base.BaseComponent;
 
 public class CardComponent implements BaseComponent {
-    private CardEntity cardEntity;
+    public String symbol;
+    public String image;
+    public String type;
+
+    private final CardEntity cardEntity;
 
     public CardComponent(CardEntity cardEntity) {
         this.cardEntity = cardEntity;
